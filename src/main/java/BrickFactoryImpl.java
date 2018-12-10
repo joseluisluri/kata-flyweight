d6e57f82441c23dc4f2b66a56b650ef3bce653bd
José Luis Luri <jose.luis.luri@gmail.com>
2018-12-10 20:01:25 +0100

    add solution

--- src/main/java/BrickFactoryImpl.java
+++ src/main/java/BrickFactoryImpl.java
@@ -1,15 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class BrickFactoryImpl implements BrickFactory {
 
-    // Begin editing here
-
-    // End editing here
+    private Map<Integer, Brick> bricks = new HashMap<>();
 
     public Brick getBrick(BrickType type, BrickColor color) {
-        // Begin editing here
+        // Find instance if exists
+        Integer hash = Objects.hash(type, color);
+        Brick brick = bricks.get(hash);
 
-        return null;
-        // End editing here
+        // If not exists, create once and store it
+        if (Objects.isNull(brick)) {
+            switch (type) {
+                case SQUARE:
+                    brick = new Brick(2, 2, color);
+                    break;
+                case RECTANGLE:
+                    brick = new Brick(4, 2, color);
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Brick type not implemented");
+            }
+
+            bricks.put(hash, brick);
+        }
+
+        return brick;
     }
 
 }
-
